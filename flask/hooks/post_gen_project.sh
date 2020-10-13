90eb3f1babd71393136cead67d0da9c7710e7ce7
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2020-10-13 08:36:07 +0700

    FLASK+FASTAPI: * add image registry field and push rule in Make file * fix git init and support bitbucket style

--- flask/hooks/post_gen_project.sh
+++ flask/hooks/post_gen_project.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+{% if cookiecutter.git_provider in ["bitbucket.org"] %}
+    GIT_URL=https://{{cookiecutter.repo_user}}@{{cookiecutter.git_provider}}/{{cookiecutter.repo_user}}/{{cookiecutter.repo_name}}.git
+{% else %}
+    GIT_URL=https://{{cookiecutter.git_provider}}/{{cookiecutter.repo_user}}/{{cookiecutter.repo_name}}
+{% endif %}
+
 source ../../hooks/post_gen_project.sh
 
-git_init
+git_init $GIT_URL
 
 run_test_and_check_lint
