b89358f7b6f9250dfb6033902d13706843f0c866
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-02-01 07:55:23 +0700

    Fix post hook

--- fastapi-postgresql/hooks/post_gen_project.sh
+++ fastapi-postgresql/hooks/post_gen_project.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+git_init() {
+    local GIT_URL=$1
+    git init
+    git remote add origin $GIT_URL
+    git add .
+    git commit -m "Initial"
+}
+
+
+run_test_and_check_lint() {
+    python -mvenv venv && source venv/bin/activate
+    make test
+    make lint
+}
+
 {% if cookiecutter.git_provider in ["bitbucket.org"] %}
     GIT_URL=https://{{cookiecutter.repo_user}}@{{cookiecutter.git_provider}}/{{cookiecutter.repo_user}}/{{cookiecutter.repo_name}}.git
 {% else %}
